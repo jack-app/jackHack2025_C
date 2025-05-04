5194c296744aa198f31a60f164ed65482fe35eff
souta0812 <168318107+souta0812@users.noreply.github.com>
2025-05-04 08:38:17 +0000

    modal window

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,29 @@
-export default function Page() {
+"use client";
+import React, { useState } from 'react';
+import Modal from './Modal';
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
   return (
+    <>
     <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>This is a simple page.</p>
+      <button onClick={()=>{
+        setIsModalOpen(true);
+      }}>Open Modal</button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <div className="gridgrid-item_1_icon">かお</div>
+        <div className="gridgrid-item_2_profile1">なまえ</div>
+        <div className="gridgrid-item_3_profile2">キャンセル</div>
+      </Modal>
+
     </div>
+    <div>
+      
+    </div>
+    </>
   );
-}
+};
+
+export default App;
