f316a038588447b23f4f386118f376d724b55bc1
vyuma <bohda.yuuma.f5@s.mail.nagoya-u.ac.jp>
2025-05-06 03:03:02 +0000

    LPpege

--- front/src/components/Startbutton.tsx
+++ front/src/components/Startbutton.tsx
@@ -10,8 +10,7 @@ import { useRouter } from "next/navigation";
     return (
       <button
         onClick={handleClick}
-        style={{ backgroundColor: "#55AD9B" }}
-        className="text-lg text-white px-10 py-3 rounded-2xl"
+        className="text-lg bg-green-400 text-white px-10 py-3 rounded-2xl hover:bg-green-600 transition duration-300 ease-in-out shadow-md hover:shadow-lg"
       >
         怠惰になる！
       </button>
