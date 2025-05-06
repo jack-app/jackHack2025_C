8431fc32e8a122d5808d56419275c29eeecb7629
mn0803 <watervalley83@gmail.com>
2025-05-06 04:52:14 +0000

    picture add

--- front/src/app/test/page.tsx
+++ front/src/app/test/page.tsx
@@ -2,12 +2,13 @@
 import React, { useState,useEffect } from "react";
 
 import Illustration from "@/components/Illustration"
+import Picture from "@/components/Picture";
 
 export default function Test() {
     const level = -300
     return (
         <>
-         <Illustration/>
+         <Picture/>
         </>
     );
 }
