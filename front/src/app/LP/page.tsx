03842b6149166f321184216d9f85068d22f0cd9c
vyuma <bohda.yuuma.f5@s.mail.nagoya-u.ac.jp>
2025-05-06 03:11:37 +0000

    any hanting

--- front/src/app/LP/page.tsx
+++ front/src/app/LP/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useState,useEffect } from "react";
+import React from "react";
 
 import Illustration from "@/components/Illustration"
 
 export default function Test() {
-    const level = -300
     return (
         <>
          <Illustration/>
